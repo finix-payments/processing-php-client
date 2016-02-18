727d17c44299c1d3f8b2bca6d40df3f7f2252cce
Finix <dev+github@finixpayments.com>
2016-02-17 16:18:53 -0800

    init disputes

---
+++ src/Finix/Resources/Dispute.php
@@ -0,0 +1,14 @@
+<?php
+namespace Finix\Resources;
+
+use Finix\Hal\HrefSpec;
+use Finix\Resource;
+
+class Dispute extends Resource
+{
+    public static function init()
+    {
+        self::getRegistry()->add(get_called_class(), new HrefSpec('disputes', 'id', '/'));
+    }
+
+}
