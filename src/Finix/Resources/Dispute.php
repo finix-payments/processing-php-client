339d16a4d31b47c3f1d4a3381b20d6a0db5795e3
Finix <dev+github@finixpayments.com>
2016-02-17 16:39:10 -0800

    add the file resource and a method to upload a file in the dispute model

--- src/Finix/Resources/Dispute.php
+++ src/Finix/Resources/Dispute.php
@@ -2,6 +2,7 @@
 namespace Finix\Resources;
 
 use Finix\Hal\HrefSpec;
+use Finix\Http\Request;
 use Finix\Resource;
 
 class Dispute extends Resource
@@ -9,6 +10,23 @@ class Dispute extends Resource
     public static function init()
     {
         self::getRegistry()->add(get_called_class(), new HrefSpec('disputes', 'id', '/'));
+    }
+
+    public function uploadFile($filePath)
+    {
+        $realFilePath = realpath($filePath);
+        if(!file_exists($realFilePath)) {
+            throw new \Exception(sprintf("File %s does not exist", $realFilePath));
+        }
+        $request = new Request(
+            $this->resource->getLink("files")->getHref(),
+            'POST',
+            ["file"=> fopen($realFilePath, 'rb')],
+            array('Content-Type => multipart/form-data')
+        );
+        $response = $this->getClient()->sendRequest($request);
+        $file = new File($response->getState(), $response->getAllLinks());
+        return $file;
     }
 
 }
