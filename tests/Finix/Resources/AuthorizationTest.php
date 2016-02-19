cc446da60f60c73224eb26568163bd3fe9b52f32
richie serna <richie@balancedpayments.com>
2016-02-19 15:39:14 -0800

    new merchant in auth test

--- tests/Finix/Resources/AuthorizationTest.php
+++ tests/Finix/Resources/AuthorizationTest.php
@@ -41,6 +41,7 @@ TAG;
     public static function setUpBeforeClass()
     {
         self::$identity = Identity::retrieve('IDjBjyepZh7pqVU1B3si4aD3');
+        self::$identity_2 = Identity::retrieve('ID623YcUS26vPJk7ctf7HREg');
         // TODO: identity must have a merchant account on DUMMY_V1 processor
         // TODO: if we're calling the api with wrong credentials, we get 500 instead of 403
         $card = json_decode(self::PAYMENT_CARD_PAYLOAD, true);
@@ -49,17 +50,33 @@ TAG;
         $card->save();
         self::$card = $card;
 
+        $card_2 = json_decode(self::PAYMENT_CARD_PAYLOAD, true);
+        $card_2['identity'] = self::$identity_2->id;
+        $card_2 = new PaymentInstrument($card_2);
+        $card_2->save();
+        self::$card_2 = $card_2;
+
     }
 
     public function setUp() {
         $this->auth = json_decode(self::AUTHORIZATION_PAYLOAD, true);
         $this->assertEquals(json_last_error(), 0);
+
+
     }
 
     private function fillAuthorization($auth)
     {
         $auth['merchant_identity'] = self::$identity->id;
         $auth['source'] = self::$card->id;
+        return $auth;
+    }
+
+
+    private function fillAuthorization_2($auth)
+    {
+        $auth['merchant_identity'] = self::$identity_2->id;
+        $auth['source'] = self::$card_2->id;
         return $auth;
     }
 
@@ -72,7 +89,7 @@ TAG;
     }
 
     public function test_updateAuthorization() {
-        $auth_state = $this->fillAuthorization($this->auth);
+        $auth_state = $this->fillAuthorization_2($this->auth);
         $auth = new Authorization($auth_state);
         $auth->save();
         $old_id = $auth->id;
