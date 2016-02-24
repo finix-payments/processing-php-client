4602b3694d9210293c543bc8eedb4ccf53e6e27f
richie serna <richie@balancedpayments.com>
2016-02-23 21:06:28 -0800

    change credentials

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,8 @@
 require(__DIR__ . '/src/Payline/Settings.php');
 Payline\Settings::configure(
     'https://api-test.payline.io',
-    'USo7wNx4M4NAFZzPWQMLBHpM',
-    '48919985-f2b2-4f42-a98f-b367dfc1316f');
+    'US6DsJ1V7qFZhFx27wuntKf1',
+    '5a363493-700d-4a03-8dce-dea2484a52df');
 
 require(__DIR__ . '/src/Payline/Bootstrap.php');
 \Payline\Bootstrap::init();
