62045cc3ea802c3d45529ecc366a5f29a60daa87
richie serna <richie@balancedpayments.com>
2016-03-01 00:38:21 -0800

    Add tests

--- tests/Finix/Resources/SettlementTest.php
+++ tests/Finix/Resources/SettlementTest.php
@@ -26,53 +26,103 @@ class SettlementTest extends \PHPUnit_Framework_TestCase
     }
 TAG;
 
-    const AUTHORIZATION_PAYLOAD = <<<TAG
+    const TRANSFER_PAYLOAD = <<<TAG
     {
-       "tags": {
-           "name": "test-merchant"
-       },
-       "processor": "DUMMY_V1",
-       "amount": 100
+        "amount": 1000,
+        "currency": "USD",
+        "processor": "DUMMY_V1"
     }
 TAG;
+
+
+    const IDENTITY_PAYLOAD = <<<TAG
+    {
+        "entity": {
+            "business_type": "LIMITED_LIABILITY_COMPANY",
+            "business_phone": "+1 (408) 756-4497",
+            "first_name": "dwayne",
+            "last_name": "Sunkhronos",
+            "dob": {
+                "month": 5,
+                "day": 27,
+                "year": 1978
+            },
+            "business_address": {
+                "city": "San Mateo",
+                "country": "USA",
+                "region": "CA",
+                "line2": "Apartment 8",
+                "line1": "741 Douglass St",
+                "postal_code": "94114"
+            },
+            "doing_business_as": "doingBusinessAs",
+            "phone": "1234567890",
+            "personal_address": {
+                "city": "San Mateo",
+                "country": "USA",
+                "region": "CA",
+                "line2": "Apartment 7",
+                "line1": "741 Douglass St",
+                "postal_code": "94114"
+            },
+            "business_name": "business inc",
+            "business_tax_id": "123456789",
+            "email": "user@example.org",
+            "tax_id": "5779"
+        }
+    }
+TAG;
+
+
+    const SETTLEMENT_PAYLOAD = <<<TAG
+    {
+        "identity": "null",
+        "processor": "DUMMY_V1",
+        "currency": "USD"
+    }
+TAG;
+
     protected static $identity;
     protected static $card;
-    protected $auth;
+    protected $settlement;
+    protected $state;
 
     public static function setUpBeforeClass()
     {
-        self::$identity = Identity::retrieve('IDjBjyepZh7pqVU1B3si4aD3');
-        // TODO: identity must have a merchant account on DUMMY_V1 processor
-        // TODO: if we're calling the api with wrong credentials, we get 500 instead of 403
+        $identity = json_decode(self::IDENTITY_PAYLOAD, true);
+        $identity = new Identity($identity);
+        self::$identity->save();
+        $identity->provisionMerchantOn("DUMMY_V1");
+
         $card = json_decode(self::PAYMENT_CARD_PAYLOAD, true);
-        $card['identity'] = self::$identity->id;
+        $card['identity'] = $identity->id;
         $card = new PaymentInstrument($card);
         $card->save();
         self::$card = $card;
 
+        $transfer = json_decode(self::TRANSFER_PAYLOAD, true);
+        $transfer = new Transfer($transfer);
+        $transfer->save();
+        self::$transfer= $transfer;
     }
 
     public function setUp() {
-        $this->auth = json_decode(self::AUTHORIZATION_PAYLOAD, true);
+        $this->settlement = json_decode(self::SETTLEMENT_PAYLOAD, true);
         $this->assertEquals(json_last_error(), 0);
     }
 
-    private function fillAuthorization($auth)
+    private function fillSettlement($settlement)
     {
-        $auth['merchant_identity'] = self::$identity->id;
-        $auth['source'] = self::$card->id;
-        return $auth;
+        $settlement['identity'] = self::$identity->id;
+        return $settlement;
     }
 
-    public function test_updateAuthorization() {
-        $auth_state = $this->fillAuthorization($this->auth);
-        $auth = new Authorization($auth_state);
-        $auth->save();
-        $old_id = $auth->id;
-        $auth->capture_amount = 50;
-        $auth->save();
-
-        $settlement = new Settlement(self::$identity->id);
-
+    public function test_createSettlement() {
+        sleep(60);
+        $settlement_state = $this->fillSettlement($this->$settlement);
+        $settlement = self::$identity->createSettlement($settlement_state);
+        $this->assertStringStartsWith('ST', $settlement->id);
+        $this->assertEquals($settlement->identity, self::$identity->id);
+        $this->assertEquals($settlement->transfer, null);
     }
 }
