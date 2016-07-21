20b4defe0a51c99a6dea33bea21b96297847541f
Phuong H <phuonghqh@gmail.com>
2016-07-22 00:47:19 +0700

    Update new version

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,8 @@ require(__DIR__ . '/src/Finix/Settings.php');
 Finix\Settings::configure(
     'https://api-staging.finix.io/',
     'USkmuYs3Sb2kcryiicgHbxGE',
-    '54fde51b-6031-4118-acf0-72f4374e8972');
+    '54fde51b-6031-4118-acf0-72f4374e8972'
+);
 
 require(__DIR__ . '/src/Finix/Bootstrap.php');
 \Finix\Bootstrap::init();
