eb2808fb075c28d0eb13ee4344c73751ba7cdd32
phuonghqh <phuonghqh@gmail.com>
2016-12-23 00:48:15 +0700

    Remove payment_instruments in updates root

--- src/Finix/Resources/InstrumentUpdate.php
+++ src/Finix/Resources/InstrumentUpdate.php
@@ -9,6 +9,6 @@ class InstrumentUpdate extends Resource
 {
     public static function init()
     {
-        self::getRegistry()->add(get_called_class(), new HrefSpec('payment_instruments/%s/updates', 'id', '/'));
+        self::getRegistry()->add(get_called_class(), new HrefSpec('updates', 'id', '/'));
     }
 }
